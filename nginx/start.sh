590dd3b7ec8fba2876c3f614d2fb2186bf98cdd3
ifdancoder <ifdancoder@gmail.com>
2025-09-28 22:12:16 +0800

    feat: nginx launcher had been updated

--- nginx/start.sh
+++ nginx/start.sh
@@ -3,6 +3,3 @@ set -e
 
 # Подставляем переменные в конфиг
 envsubst '$NGINX_HOST' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Запускаем nginx
-exec nginx -g 'daemon off;'
